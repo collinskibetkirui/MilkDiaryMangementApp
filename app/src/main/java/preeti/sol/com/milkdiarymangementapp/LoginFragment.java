8e798ffb208853b722e623938c2c0eb4837ee9cf
shahupreeti <smartcoders25@gmail.com>
2018-06-06 20:32:04 +0530

    first commit..

---
+++ app/src/main/java/preeti/sol/com/milkdiarymangementapp/LoginFragment.java
@@ -0,0 +1,50 @@
+package preeti.sol.com.milkdiarymangementapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+Button loginButton;
+TextView signupTextView;
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        loginButton = (Button) view.findViewById(R.id.login_button);
+        signupTextView = (TextView) view.findViewById(R.id.signup_textView2);
+
+        signupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment signupFragment = new SignUpFragment();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.root_layout,signupFragment);
+                ft.commit();
+            }
+        });
+        return view;
+    }
+
+}
---
+++ app/src/main/java/preeti/sol/com/milkdiarymangementapp/MainActivity.java
@@ -0,0 +1,24 @@
+package preeti.sol.com.milkdiarymangementapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      LoginFragment loginFragment = new LoginFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.root_layout,loginFragment);
+        ft.commit();
+    }
+}
